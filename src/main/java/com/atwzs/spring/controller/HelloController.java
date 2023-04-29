1911860b8d67547f5112e1cc4979049dce7c0b34
小王 <3517139394@qq.com>
2023-04-29 11:23:00 +0800

    第si次提交至本地库

--- src/main/java/com/atwzs/spring/controller/HelloController.java
+++ src/main/java/com/atwzs/spring/controller/HelloController.java
@@ -18,7 +18,7 @@ public class HelloController {
 
     @RequestMapping(value = "/", params = {"username", "password=123"})
     public String login() {
-        System.out.println("2345t43");
+        System.out.println("2345t435678976");
         return "login";
     }
 
