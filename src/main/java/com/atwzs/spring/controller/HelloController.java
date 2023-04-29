d52bc800e2ef90c25245f6d521af99dcc5da939a
abandkonee <132114918+abandkonee@users.noreply.github.com>
2023-04-29 11:42:17 +0800

    Update HelloController.java

--- src/main/java/com/atwzs/spring/controller/HelloController.java
+++ src/main/java/com/atwzs/spring/controller/HelloController.java
@@ -18,7 +18,7 @@ public class HelloController {
 
     @RequestMapping(value = "/", params = {"username", "password=123"})
     public String login() {
-        System.out.println("杨幂流泪,test1111");
+        System.out.println("杨幂流泪,test1111,只是长得有点像而已");
         return "login";
     }
 
